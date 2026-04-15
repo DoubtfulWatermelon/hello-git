cb088ae350dbb998b81cb29692f21a89f6cb068c
Azu ‎ <azrsyh57@gmail.com>
2026-04-15 22:49:13 +0800

    Added for loop

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ int main()
 {
     cout<< "Hello, Giga!"<< endl;
     cout<< "I Love C++";
+    for(int i= 1; i<= 3; ++i){
+        cout<<" Hello, Giga! This is line "<< i << ". "<< endl;
+    }
     return 0;
 }
